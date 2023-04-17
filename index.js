a8d236b3e0bb6d2c4eb2e99973579b622eb39ad0
tasnubamaha <tasnubamaha267@gmail.com>
2023-04-17 12:50:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const phones = require('./phones.json');
+const app = express();
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+  res.send('my phone information coming soon')
+})
+
+app.get('/phones', (req, res) => {
+  res.send('phones');
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id: ', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phones);
+})
+
+
+app.listen(port, () => {
+    console.log(`my phone server is running on port: ${port}`)
+})
